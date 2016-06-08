91a14d32fb4134393c51857cb66fac9c7b913458
whx405831799 <whx405831799@gmail.com>
2016-06-08 16:46:20 +0800

    秒杀web

---
+++ src/main/java/org/seckill/web/SeckillController.java
@@ -0,0 +1,88 @@
+package org.seckill.web;
+
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
+import org.seckill.dto.SeckillResult;
+import org.seckill.entity.Seckill;
+import org.seckill.enums.SeckillStatEnum;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
+import org.seckill.exception.SeckillException;
+import org.seckill.service.SeckillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/6/8.
+ */
+@Controller
+@RequestMapping("/seckill")//module 模块/资源/id/细分
+public class SeckillController {
+    @Autowired
+    private SeckillService seckillService;
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+
+    public String getList(Model model) {
+        //获取列表页
+        List list = seckillService.getSeckillList();
+        model.addAttribute("list", list);
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String getDetail(@PathVariable("seckillId") Long seckillId, Model model) {
+        if (seckillId == null) {
+            return "redirect:/seckill/list";
+        }
+        Seckill seckill = seckillService.getById(seckillId);
+        if (seckill == null) {
+            return "forword:/seckill/list";
+        }
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(@PathVariable("seckillId") Long seckillId) {
+
+        SeckillResult<Exposer> seckillResult;
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(seckillId);
+            seckillResult = new SeckillResult<Exposer>(true, exposer);
+        } catch (Exception e) {
+            e.printStackTrace();
+            seckillResult = new SeckillResult<Exposer>(false, e.getMessage());
+        }
+        return seckillResult;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST, produces = {"application/json;charset=UTF-8"})
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(@PathVariable("seckillId") Long seckillId, @CookieValue(value = "killPhone", required = false) Long userPhone, @PathVariable("md5") String md5) {
+        SeckillResult seckillResult;
+        try {
+            //可用springmvc validate
+            if(userPhone == null){
+                return seckillResult = new SeckillResult(false,"未注册");
+            }
+            SeckillExecution seckillExecution = seckillService.executeSeckill(seckillId, userPhone, md5);
+            seckillResult = new SeckillResult(true, seckillExecution);
+        } catch (RepeatKillException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<SeckillExecution>(false, seckillExecution);
+        }catch (SeckillCloseException e){
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.END);
+            return new SeckillResult<SeckillExecution>(false, seckillExecution);
+        }catch (Exception e) {
+            SeckillExecution seckillExecution = new SeckillExecution(seckillId, SeckillStatEnum.INNER_ERROR);
+            return new SeckillResult<SeckillExecution>(false, seckillExecution);
+        }
+        return seckillResult;
+    }
+}
